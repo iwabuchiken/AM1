b11082ab39ed91de92ab5da7145d4b387a34ac3f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-08-01 12:39:16 +0900

    master
    v-1.0
    
    D---------------------
    1. Initial

---
+++ src/am1/main/AM1Activity.java
@@ -0,0 +1,13 @@
+package am1.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AM1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
